24773080bae90b52a3f574f80462512f1e0ef210
DergerNox <ferinko10@gmail.com>
2025-08-03 21:22:35 +0200

    added routing and modified app.tsx in preparation for a Videoviewer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import VideoList from './components/VideoList';
 import VideoUpload from './components/VideoUpload';
+import WatchPage from './components/WatchPage';
 
 const App: React.FC = () => {
   return (
-    <div style={{ padding: '20px' }}>
-      <VideoUpload />
-      <hr />
-      <VideoList />
-    </div>
+    <Router>
+      <div style={{ padding: '20px' }}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <VideoUpload />
+                <hr />
+                <VideoList />
+              </>
+            }
+          />
+          <Route path="/watch/:id" element={<WatchPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
-
 
 export default App;
