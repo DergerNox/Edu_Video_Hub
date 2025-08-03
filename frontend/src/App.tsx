0d85ccc869f18ae628e502b1ce6839ced8b28f98
DergerNox <ferinko10@gmail.com>
2025-08-03 17:20:41 +0200

    updated App.tsx to acomodate Video uplad

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import VideoList from './components/VideoList';
+import VideoUpload from './components/VideoUpload';
 
 const App: React.FC = () => {
   return (
     <div style={{ padding: '20px' }}>
+      <VideoUpload />
+      <hr />
       <VideoList />
     </div>
   );
 };
+
 
 export default App;
