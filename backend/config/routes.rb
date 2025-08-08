fb3d7c62741f6124aaeec0f6653d639f42ba8fba
DergerNox <ferinko10@gmail.com>
2025-08-08 17:07:04 +0200

    Fix indentation and formatting in routes.rb for better readability

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,15 +1,16 @@
-namespace :api do
-  devise_for :users,
-             path: '',
-             path_names: {
-               sign_in: 'login',
-               sign_out: 'logout',
-               registration: 'signup'
-             },
-             controllers: {
-               sessions: 'api/sessions',
-               registrations: 'api/registrations'
-             }
-
-  resources :videos, only: [:index, :show, :create]
+Rails.application.routes.draw do
+  namespace :api do
+    devise_for :users,
+      path: '',
+      path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'signup'
+      },
+      controllers: {
+        sessions: 'api/sessions',
+        registrations: 'api/registrations'
+      }
+    resources :videos, only: [:index, :show, :create]
+  end
 end
