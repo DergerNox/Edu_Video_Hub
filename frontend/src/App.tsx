1906a1cbd07edf88ecead3d346e51405d94cfe70
DergerNox <ferinko10@gmail.com>
2025-08-08 18:05:13 +0200

    Wrap App component with AuthProvider for authentication context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import VideoList from './components/VideoList';
 import VideoUpload from './components/VideoUpload';
 import WatchPage from './components/WatchPage';
+import { AuthProvider } from './context/AuthContext';
+import ReactDOM from 'react-dom/client'; // For React 18+ with createRoot
+
 
 const App: React.FC = () => {
   return (
@@ -24,6 +27,14 @@ const App: React.FC = () => {
       </div>
     </Router>
   );
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
 };
+
 
 export default App;
