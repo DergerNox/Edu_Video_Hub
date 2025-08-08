51484ac1a812b48dae996686ad5db813762e5eb2
DergerNox <ferinko10@gmail.com>
2025-08-08 17:36:12 +0200

    Implement respond_with method for handling registration responses in JSON format

--- backend/app/controllers/api/registrations_controller.rb
+++ backend/app/controllers/api/registrations_controller.rb
@@ -1,2 +1,13 @@
 class Api::RegistrationsController < ApplicationController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: { message: 'Signed up successfully.', user: resource }, status: :ok
+    else
+      render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
